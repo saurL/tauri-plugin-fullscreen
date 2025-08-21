4dd6d32d9caeed2025b936b59a04df634fbd2301
lucas <sauronlucas@gmail.com>
2025-08-21 14:42:55 +0200

    a

--- ios/Sources/FullscreenPlugin.swift
+++ ios/Sources/FullscreenPlugin.swift
@@ -6,39 +6,20 @@ class FullscreenPlugin: Plugin {
     private var webviewRef: WKWebView?
 
     @objc open override func load(webview: WKWebView) {
-        // On garde la référence
-        self.webviewRef = webview
-
-        // Récupération safe du rootViewController
-        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-              let keyWindow = windowScene.windows.first(where: { $0.isKeyWindow }) ?? UIApplication.shared.windows.first,
-              let rootVC = keyWindow.rootViewController else { return }
-
-        // Config Fullscreen
+     guard let rootVC = UIApplication.shared.keyWindow?.rootViewController else { return }
+        
         rootVC.edgesForExtendedLayout = .all
         rootVC.extendedLayoutIncludesOpaqueBars = true
-
+        
         webview.backgroundColor = .clear
         webview.scrollView.backgroundColor = .clear
-        webview.scrollView.contentInsetAdjustmentBehavior = .automatic // <-- iOS gère le clavier
-        webview.isUserInteractionEnabled = true
+        webview.scrollView.contentInsetAdjustmentBehavior = .automatic
+        
+        webview.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        webview.frame = rootVC.view.bounds
+        
+        UIApplication.shared.keyWindow?.backgroundColor = .white
 
-        // Ajout safe du WebView
-        if webview.superview == nil {
-            rootVC.view.addSubview(webview)
-        }
-
-        // Auto Layout pour fullscreen
-        webview.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            webview.topAnchor.constraint(equalTo: rootVC.view.topAnchor),
-            webview.bottomAnchor.constraint(equalTo: rootVC.view.bottomAnchor),
-            webview.leadingAnchor.constraint(equalTo: rootVC.view.leadingAnchor),
-            webview.trailingAnchor.constraint(equalTo: rootVC.view.trailingAnchor)
-        ])
-
-        // Background window
-        keyWindow.backgroundColor = .white
     }
 }
 
