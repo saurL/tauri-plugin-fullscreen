427d7a8ef3480ca971801b7d44bda5daadbe57a6
lucas <sauronlucas@gmail.com>
2025-08-21 12:12:27 +0200

    added swift pacakge

---
+++ ios/Sources/FullscreenPlugin.swift
@@ -0,0 +1,28 @@
+import SwiftRs
+import Tauri
+import UIKit
+import WebKit
+
+
+class FullscreenPlugin: Plugin {
+   @objc open override func load(webview: WKWebView) {
+        guard let rootVC = UIApplication.shared.keyWindow?.rootViewController else { return }
+        
+        rootVC.edgesForExtendedLayout = .all
+        rootVC.extendedLayoutIncludesOpaqueBars = true
+        
+        webview.backgroundColor = .clear
+        webview.scrollView.backgroundColor = .clear
+        webview.scrollView.contentInsetAdjustmentBehavior = .never
+        
+        webview.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        webview.frame = rootVC.view.bounds
+        
+        UIApplication.shared.keyWindow?.backgroundColor = .white
+    }
+}
+
+@_cdecl("init_plugin_fullscreen")
+func initPlugin() -> Plugin {
+  return FullscreenPlugin()
+}
