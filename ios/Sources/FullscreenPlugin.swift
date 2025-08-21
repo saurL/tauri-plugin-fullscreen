94d59f1224d00542429bb41dd728f3bdd43cb38e
lucas <sauronlucas@gmail.com>
2025-08-21 14:31:50 +0200

    a

--- ios/Sources/FullscreenPlugin.swift
+++ ios/Sources/FullscreenPlugin.swift
@@ -7,21 +7,29 @@ class FullscreenPlugin: Plugin {
     private var webviewRef: WKWebView?
 
     @objc open override func load(webview: WKWebView) {
+        // On garde la référence
         self.webviewRef = webview
 
+        // Récupération safe du rootViewController
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-              let keyWindow = windowScene.windows.first(where: { $0.isKeyWindow }),
+              let keyWindow = windowScene.windows.first(where: { $0.isKeyWindow }) ?? UIApplication.shared.windows.first,
               let rootVC = keyWindow.rootViewController else { return }
 
+        // Config Fullscreen
         rootVC.edgesForExtendedLayout = .all
         rootVC.extendedLayoutIncludesOpaqueBars = true
 
         webview.backgroundColor = .clear
         webview.scrollView.backgroundColor = .clear
-        webview.scrollView.contentInsetAdjustmentBehavior = .automatic  // <-- iOS gère le clavier
+        webview.scrollView.contentInsetAdjustmentBehavior = .automatic
         webview.isUserInteractionEnabled = true
 
-        rootVC.view.addSubview(webview)
+        // Ajout safe du WebView
+        if webview.superview == nil {
+            rootVC.view.addSubview(webview)
+        }
+
+        // Auto Layout pour fullscreen
         webview.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             webview.topAnchor.constraint(equalTo: rootVC.view.topAnchor),
@@ -30,9 +38,11 @@ class FullscreenPlugin: Plugin {
             webview.trailingAnchor.constraint(equalTo: rootVC.view.trailingAnchor)
         ])
 
+        // Background window
         keyWindow.backgroundColor = .white
     }
 }
+
 
 @_cdecl("init_plugin_fullscreen")
 func initPlugin() -> Plugin {
